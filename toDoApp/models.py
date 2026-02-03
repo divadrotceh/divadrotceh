28636ab5206f5ea78bd9e46be33866098d6a51cc
divadrotceh <divadrotceh@icloud.com>
2026-02-03 13:16:07 -0600

    Update models.py

--- toDoApp/models.py
+++ toDoApp/models.py
@@ -1,1 +1,21 @@
+from typing import List
+from typing import Optional
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
 
+class Base(DeclarativeBase):
+  pass
+
+class User(Base):
+  #Table Name
+  __tablename__ = "user_account"
+
+  #Columns
+  name: 
+  
+  def __repr__(self):
+    pass
