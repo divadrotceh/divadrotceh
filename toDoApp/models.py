74769d1aff1ec4928bbbe60c14b3219a7a88c3f3
divadrotceh <divadrotceh@icloud.com>
2026-02-05 14:55:54 -0600

    Update models.py
    creating tables for users and tasks

--- toDoApp/models.py
+++ toDoApp/models.py
@@ -6,6 +6,8 @@ from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import relationship
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
 
 class Base(DeclarativeBase):
   pass
@@ -13,9 +15,24 @@ class Base(DeclarativeBase):
 class User(Base):
   #Table Name
   __tablename__ = "user_account"
-
+  
   #Columns
-  name: 
+  user_name: Mapped(str)
+  password_hash: Mapped(str)
+  id = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
   
   def __repr__(self):
-    pass
+    return f"User Name: {self.user_name}, id: {self.id}"
+
+class Task(Base):
+  #Table Name
+  __tablename__ = "tasks"
+
+  #Comlumns
+  task_name: Mapped(str)
+  id = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+  priority: Mapped(str)
+  user_id: Mapped(int) = mapped_column(ForeignKey("user_account.id")) 
+
+  def __repr__(self):
+    return "Task Name: {task_name}, id: {id}, priority: {priority}"
