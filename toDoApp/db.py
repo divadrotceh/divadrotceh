92207be7460ae9287f665ff4511ff99fc824c652
divadrotceh <divadrotceh@icloud.com>
2026-02-09 13:54:57 -0600

    Update db.py

--- toDoApp/db.py
+++ toDoApp/db.py
@@ -1,3 +1,34 @@
 from sqlalchemy import create_engine
-
 engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
+
+def create_tables(Base):
+  """
+  This is to initialize the database tables.
+  it is supposed to be called from outside and create the
+  tables of metadata extracted from the Base object passed
+  as parameter.
+  """
+  base.metadata.create_all(engine)
+
+def add_user(User):
+  pass
+
+def remove_user(user_id):
+  pass
+
+def add_task(Task):
+  pass
+
+def remove_task(task_id):
+  pass
+
+def find_user(user_id):
+  pass
+
+def edit_task(task_id):
+  pass
+
+def edit_user(user_id):
+  pass
+
+
