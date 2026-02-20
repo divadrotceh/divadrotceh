1f0633b22469dc4826df74a64688c731d12e5319
divadrotceh <divadrotceh@icloud.com>
2026-02-19 21:53:08 -0600

    Add blueprint registration and secret key to app

--- ToDo_flask/main.py
+++ ToDo_flask/main.py
@@ -1,4 +1,7 @@
 from app import app
+from views import bp
 
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.secret_key = "jdkfosvns"
+    app.register_blueprint(bp)
+    app.run(debug = True)
